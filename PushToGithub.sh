fbd6acfdda210b0b3b88ec3af0bbb3707257bb7e
wadhwarochak <rochak.wadhwa@gmail.com>
2025-12-23 16:57:51 +0530

    Updated recent changes

--- PushToGithub.sh
+++ PushToGithub.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-rm -rf MyLinuxMachi*
-rm -rf SCRIP*
-git clone https://github.com/wadhwarochak/MyLinuxMachine.git
-
-cp -r /d/CODE/zGithub-Repository/Automation-Utilities-In-Bash/MyLinuxMachine/SCR* /d/CODE/zGithub-Repository/Automation-Utilities-In-Bash/
-
-rm -rf MyLinuxMachi*
-
 
 # Script: push_to_github.sh
 # Purpose: Commit and push recent changes to GitHub
