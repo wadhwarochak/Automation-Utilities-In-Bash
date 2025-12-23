dbed2f73598071c5a2dd5fdcff8e4ce60d449898
wadhwarochak <rochak.wadhwa@gmail.com>
2025-12-23 14:49:30 +0530

    Updated recent changes

---
+++ getScripts.sh
@@ -0,0 +1,7 @@
+rm -rf MyLinuxMachi*
+rm -rf SCRIP*
+git clone https://github.com/wadhwarochak/MyLinuxMachine.git
+
+cp -r /d/CODE/zGithub-Repository/Automation-Utilities-In-Bash/MyLinuxMachine/SCR* /d/CODE/zGithub-Repository/Automation-Utilities-In-Bash/
+
+rm -rf MyLinuxMachi*
